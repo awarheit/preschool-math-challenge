3e1079883be90a1d95a14d68f9dc75a5e34df8f0
Anna Warheit <anna.r.warheit@gmail.com>
2021-05-19 18:02:56 -0400

    update js functions to check for correct answer and clear input field after each submit

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+function makeRandNum() {
+    return Math.floor(Math.random() * 20) + 1;
+  }
+  // Change "20" above to different max num to adjust difficulty
+    
+var problemNum1 = 3;
+var problemNum2 = 2;
+
+function newProblem() {
+    problemNum1 = makeRandNum();
+    problemNum2 = makeRandNum();
+    const problem = document.querySelector("#problem");
+    problem.innerText = `What is ${problemNum1} + ${problemNum2}?`;
+    answerField.value = '';
+  }
+  newProblem();
+
+
 const problemForm = document.querySelector("#problemForm");
 problemForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  const answer = parseInt(problemForm.elements.answerField.value);
+  if (answer === problemNum1 + problemNum2) {
+      alert("Correct!")
+  } else {
+      alert("Wrong-o!")
+  }
+  newProblem();
 });
 
-function makeRandNum() {
-  return Math.floor(Math.random() * 20) + 1;
-}
-// Change "20" above to different max num to adjust difficulty
 
 
-function newProblem() {
-  const problemNum1 = makeRandNum();
-  const problemNum2 = makeRandNum();
-  const problem = document.querySelector("#problem");
-  problem.innerText = `What is ${problemNum1} + ${problemNum2}?`;
-}
 
-newProblem();
