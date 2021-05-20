baff8b7d9d202dd67d10c25f66fbae7a789ad85c
Anna Warheit <anna.r.warheit@gmail.com>
2021-05-20 15:09:52 -0400

    add individual correctDiv classes with index number to change correct squares to each color of the rainbow

--- app.js
+++ app.js
@@ -21,25 +21,25 @@ problemForm.addEventListener("submit", function (e) {
     const divContainer = document.querySelector("#divContainer");
     const divObject = divContainer.children;
     if (divObject[0].classList.contains("neutralDiv")) {
-      divObject[0].classList.replace("neutralDiv", "correctDiv");
+      divObject[0].classList.replace("neutralDiv", "correctDiv0");
     } else {
       if (divObject[1].classList.contains("neutralDiv")) {
-        divObject[1].classList.replace("neutralDiv", "correctDiv");
+        divObject[1].classList.replace("neutralDiv", "correctDiv1");
       } else {
         if (divObject[2].classList.contains("neutralDiv")) {
-          divObject[2].classList.replace("neutralDiv", "correctDiv");
+          divObject[2].classList.replace("neutralDiv", "correctDiv2");
         } else {
           if (divObject[3].classList.contains("neutralDiv")) {
-            divObject[3].classList.replace("neutralDiv", "correctDiv");
+            divObject[3].classList.replace("neutralDiv", "correctDiv3");
           } else {
             if (divObject[4].classList.contains("neutralDiv")) {
-              divObject[4].classList.replace("neutralDiv", "correctDiv");
+              divObject[4].classList.replace("neutralDiv", "correctDiv4");
             } else {
               if (divObject[5].classList.contains("neutralDiv")) {
-                divObject[5].classList.replace("neutralDiv", "correctDiv");
+                divObject[5].classList.replace("neutralDiv", "correctDiv5");
               } else {
                 if (divObject[6].classList.contains("neutralDiv")) {
-                  divObject[6].classList.replace("neutralDiv", "correctDiv");
+                  divObject[6].classList.replace("neutralDiv", "correctDiv6");
                   alert("You win!");
                 } else {
                   alert("you already won!");
@@ -56,26 +56,26 @@ problemForm.addEventListener("submit", function (e) {
     if (divObject[0].classList.contains("neutralDiv")) {
       alert("Try again!");
     } else {
-      if (divObject[6].classList.contains("correctDiv")) {
-        divObject[6].classList.replace("correctDiv", "neutralDiv");
+      if (divObject[6].classList.contains("correctDiv6")) {
+        divObject[6].classList.replace("correctDiv6", "neutralDiv");
       } else {
-        if (divObject[5].classList.contains("correctDiv")) {
-          divObject[5].classList.replace("correctDiv", "neutralDiv");
+        if (divObject[5].classList.contains("correctDiv5")) {
+          divObject[5].classList.replace("correctDiv5", "neutralDiv");
         } else {
-          if (divObject[4].classList.contains("correctDiv")) {
-            divObject[4].classList.replace("correctDiv", "neutralDiv");
+          if (divObject[4].classList.contains("correctDiv4")) {
+            divObject[4].classList.replace("correctDiv4", "neutralDiv");
           } else {
-            if (divObject[3].classList.contains("correctDiv")) {
-              divObject[3].classList.replace("correctDiv", "neutralDiv");
+            if (divObject[3].classList.contains("correctDiv3")) {
+              divObject[3].classList.replace("correctDiv3", "neutralDiv");
             } else {
-              if (divObject[2].classList.contains("correctDiv")) {
-                divObject[2].classList.replace("correctDiv", "neutralDiv");
+              if (divObject[2].classList.contains("correctDiv2")) {
+                divObject[2].classList.replace("correctDiv2", "neutralDiv");
               } else {
-                if (divObject[1].classList.contains("correctDiv")) {
-                  divObject[1].classList.replace("correctDiv", "neutralDiv");
+                if (divObject[1].classList.contains("correctDiv1")) {
+                  divObject[1].classList.replace("correctDiv1", "neutralDiv");
                 } else {
-                  if (divObject[0].classList.contains("correctDiv")) {
-                    divObject[0].classList.replace("correctDiv", "neutralDiv");
+                  if (divObject[0].classList.contains("correctDiv0")) {
+                    divObject[0].classList.replace("correctDiv0", "neutralDiv");
                   }
                 }
               }
